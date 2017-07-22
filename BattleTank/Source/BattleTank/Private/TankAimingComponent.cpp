b6cec64728788f88082f50e2ee450b072bf286db
Benji <benji@chezmoi.com>
2017-07-22 23:14:13 +0400

    BT15: Prework of SuggestProjectileVelocity() Addition of a LaunchSpeed UPROPERTY (EditAnywhere)

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -41,10 +41,9 @@ void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent * BarrelToSet
 	Barrel = BarrelToSet;
 }
 
-void UTankAimingComponent::AimAt(FVector HitLocation)
+void UTankAimingComponent::AimAt(FVector HitLocation,float LaunchSpeed)
 {
-	auto MyTank = GetOwner()->GetName();
-	auto BarrelLocation = Barrel->GetComponentLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s "), *MyTank, *HitLocation.ToString(),*BarrelLocation);
+
+	UE_LOG(LogTemp, Warning, TEXT("Firing at %f "),LaunchSpeed);
 }
 
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -22,6 +22,9 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere,Category=Firing)
+	float LaunchSpeed=100000; //TODO Find a sensible default
+
 public:
 	void AimAt(FVector HitLocation);
 
